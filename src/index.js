bff46fe48b0f50be5902999ae3603abe065307c1
chenyuhao <cccchenyuhao@gmail.com>
2022-03-05 17:10:49 +0800

    add style loader

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
+import "./style.css"
 import component from "./component"
 
 document.body.appendChild(component())
+
+function createLogoContainer() {
+  const logo = document.createElement("div")
+  logo.setAttribute("id", "logo")
+  return logo
+}
+document.body.appendChild(createLogoContainer())
 
 console.log("devserver hello")
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,9 @@ const HtmlWebpackPlugin = require("html-webpack-plugin")
 const { merge } = require("webpack-merge")
 
 const commonConfig = {
+  module: {
+    rules: [{ test: /\.css$/, use: ["style-loader", "css-loader"] }],
+  },
   plugins: [new HtmlWebpackPlugin()],
 }
 
